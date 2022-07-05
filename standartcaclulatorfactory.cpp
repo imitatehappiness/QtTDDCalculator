f8ae2c870ce3bc1e94a75345cee17d0dcf921031
Kalensky I.S <zivanz11@mail.ru>
2022-07-05 16:05:47 +0300

    icon

--- standartcaclulatorfactory.cpp
+++ standartcaclulatorfactory.cpp
@@ -1,23 +1,23 @@
 #include "standartcaclulatorfactory.h"
 
 QString StandartCaclulatorFactory::addition(QString first, QString second) const{
-     return QString::number( first.toDouble() + second.toDouble() );
+     return QString::number( first.toDouble() + second.toDouble(), 'g', 15 );
 }
 
 QString StandartCaclulatorFactory::subtraction(QString first, QString second) const{
-     return QString::number( first.toDouble() - second.toDouble() );
+     return QString::number( first.toDouble() - second.toDouble(), 'g', 15  );
 }
 
 QString StandartCaclulatorFactory::multiplication(QString first, QString second) const{
-     return QString::number( first.toDouble() * second.toDouble() );
+     return QString::number( first.toDouble() * second.toDouble(), 'g', 15  );
 }
 
 QString StandartCaclulatorFactory::division(QString first, QString second) const{
-    return QString::number( first.toDouble() / second.toDouble() );
+    return QString::number( first.toDouble() / second.toDouble(), 'g', 15  );
 }
 
 QString StandartCaclulatorFactory::remainder(QString first, QString second) const{
-    return QString::number( static_cast<int>(first.toDouble()) % static_cast<int>(second.toDouble()) );
+    return QString::number( static_cast<int>(first.toDouble()) % static_cast<int>(second.toDouble()), 'g', 15  );
 }
 
 QString StandartCaclulatorFactory::point(QString num) {
@@ -32,11 +32,11 @@ QString StandartCaclulatorFactory::addNum(QString num, QString bntNum) {
 }
 
 QString StandartCaclulatorFactory::reverseSign(QString num) const{
-    return QString::number(num.toDouble() * (-1));
+    return QString::number(num.toDouble() * (-1), 'g', 15 );
 }
 
 QString StandartCaclulatorFactory::oneDevide(QString num) const{
-    return QString::number(static_cast<double>(1) / num.toDouble());
+    return QString::number(static_cast<double>(1) / num.toDouble(), 'g', 15 );
 }
 
 QString StandartCaclulatorFactory::clear(QString num) const{
@@ -51,9 +51,9 @@ QString StandartCaclulatorFactory::clearAll() const{
 }
 
 QString StandartCaclulatorFactory::pow2(QString num) const{
-    return QString::number(num.toDouble() * num.toDouble());
+    return QString::number(num.toDouble() * num.toDouble(), 'g', 15 );
 }
 
 QString StandartCaclulatorFactory::sqrt2(QString num) const{
-    return QString::number(sqrt(num.toDouble()));
+    return QString::number(sqrt(num.toDouble()), 'g', 15 );
 }
