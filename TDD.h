0e7244999b899ef8db3764b61719077985947ee3
imitatehappiness <zivanz22@mail.ru>
2022-07-05 19:55:47 +0300

    tests

--- TDD.h
+++ TDD.h
@@ -21,22 +21,46 @@ private slots:
     void remainder();
 
     void addition_data();
+    void subtraction_data();
+    void multiplication_data();
 };
 
 TDDTest::TDDTest() {
 }
 
-inline void TDDTest::addition_data(){
+void TDDTest::addition_data(){
     QTest::addColumn<QString>("a");
     QTest::addColumn<QString>("b");
     QTest::addColumn<QString>("result");
 
-    QTest::newRow("test(10,1)") << "10" << "1" << "11";
-    QTest::newRow("test(10,2)") << "10" << "2" << "12";
-    QTest::newRow("test(10,3)") << "10" << "3" << "13";
-    QTest::newRow("test(10,4)") << "10" << "4" << "14";
-    QTest::newRow("test(10,5)") << "10" << "5" << "15";
-    QTest::newRow("test(10,6)") << "10" << "6" << "16";
+    QTest::newRow("(10,1)") << "10" << "1" << "11";
+    QTest::newRow("(-10,2)") << "-10" << "2" << "-8";
+    QTest::newRow("(10.2,3)") << "10.2" << "3" << "13.2";
+    QTest::newRow("(10,-4.1)") << "10" << "-4.1" << "5.9";
+    QTest::newRow("(10,-100)") << "10" << "-100" << "-90";
+}
+
+void TDDTest::subtraction_data(){
+    QTest::addColumn<QString>("a");
+    QTest::addColumn<QString>("b");
+    QTest::addColumn<QString>("result");
+
+    QTest::newRow("(10,1)") << "10" << "1" << "9";
+    QTest::newRow("(10,-2)") << "10" << "-2" << "12";
+    QTest::newRow("(10,3.2)") << "10" << "3.2" << "6.8";
+    QTest::newRow("(0,0)") << "0" << "0" << "0";
+}
+
+void TDDTest::multiplication_data(){
+    QTest::addColumn<QString>("a");
+    QTest::addColumn<QString>("b");
+    QTest::addColumn<QString>("result");
+
+    QTest::newRow("(10,1)") << "10" << "1" << "10";
+    QTest::newRow("(10,-2)") << "10" << "-2" << "-20";
+    QTest::newRow("(3.4,5.22)") << "3.4" << "5.22" << "17.748";
+    QTest::newRow("(100,0)") << "100" << "0" << "0";
+    QTest::newRow("(100,-123.3123)") << "100" << "-123.3123" << "-12331.23";
 }
 
 void TDDTest::addition() {
@@ -48,13 +72,20 @@ void TDDTest::addition() {
 }
 
 void TDDTest::subtraction() {
-    QCOMPARE(calulator.subtraction("10","13"), "-3");
+    QFETCH(QString, a);
+    QFETCH(QString, b);
+    QFETCH(QString, result);
+
+    QCOMPARE(calulator.subtraction(a, b), result);
 }
 
 void TDDTest::multiplication(){
-    QCOMPARE(calulator.multiplication("10","13"), "130");
-}
+    QFETCH(QString, a);
+    QFETCH(QString, b);
+    QFETCH(QString, result);
 
+    QCOMPARE(calulator.multiplication(a, b), result);
+}
 void TDDTest::division(){
     QCOMPARE(calulator.division("10","10"), "1");
 }
@@ -64,7 +95,7 @@ void TDDTest::remainder(){
 }
 
 #ifdef TEST
-    QTEST_APPLESS_MAIN( TDDTest )
+QTEST_APPLESS_MAIN( TDDTest )
 #endif
 
 #endif // TDD_H
